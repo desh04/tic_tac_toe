ed06e8f8ef75e10eb7dc836e1ed641a65daa3ab4
desh04 <singh.desh0404@gmail.com>
2021-08-31 22:32:02 +0530

    fix the click bug and implement the winning rule

---
+++ src/components/Game.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react'
+import Board from './Board';
+
+export default class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            xIsNext : true,
+            stepNumber : 0, 
+            history: [
+                {squares: Array(9).fill(null)}
+            ]
+        }
+        this.handleClick = this.handleClick.bind(this);
+
+    }
+    handleClick(i){
+        console.log(i);
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];       // get last item 
+        const squares = current.squares.slice();        // create a copy of squares, assgin it to variable name squares( on the left of the equla sign)
+        const winner = calculateWinner(squares);
+
+        // if winner exist then there will no change. direct return
+        // also if squares[i] exist that means player has fille the square then 
+        // that will also not create any changes so that block will not get change. 
+        if(winner || squares[i]){
+            return;
+        }
+
+        squares[i] = this.state.xIsNext ? 'X' : 'O' ;
+
+        this.setState({
+            history: history.concat({
+                squares: squares
+            }),
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
+        }) 
+/* 
+        this.setState((state)=>({
+            history: state.history.concate({
+                squares: squares
+            }) ,
+            xIsNext: !state.xIsNext,
+            stepNumber: state.history.length 
+        }))
+         */
+    }
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber]
+
+        return (
+            <div className='game'>
+                <div className='game-board'>
+                    <Board 
+                        onClick={(i)=>this.handleClick(i)} 
+                        squares={current.squares}
+                    />
+                </div>
+                
+            </div>
+        )
+    }
+}
+
+function calculateWinner(squares) {
+    const lines = [                //x or o fill these will win 
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i=0; i< lines.length ; i++){
+        const[a, b, c] = lines[i];
+        if ( squares[a] && squares[a]===squares[b] && squares[b]===squares[c]){
+            return squares[a];          // if return value is 'x' then 'x' is winner. and same for 'o'.
+        }
+    } 
+    // if return null then no winner . 
+    return null;
+}
